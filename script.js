9c4ca6101d7d8ac898e321549f64ec8cd2ad092d
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-02-04 18:27:39 +0100

    Header & Search

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function() {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+window.onclick = function (event) {
+  if (modal.style.display === "block") {
+    var modalContent = modal.querySelector(".modal-content");
+    if (!modalContent.contains(event.target)) {
+      modal.style.display = "none";
+    }
   }
 }
