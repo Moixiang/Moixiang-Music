1995a83abb7333a780d393d6cf2b6f06b5e9fb5c
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-02-04 19:32:36 +0100

    Logo ändring

--- script.js
+++ script.js
@@ -10,17 +10,8 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-window.onclick = function (event) {
-  if (modal.style.display === "block") {
-    var modalContent = modal.querySelector(".modal-content");
-    if (!modalContent.contains(event.target)) {
-      modal.style.display = "none";
-    }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
