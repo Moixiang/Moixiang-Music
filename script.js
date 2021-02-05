037ff1ac534bde8f5dab228d7fcf75e61e1f2083
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-02-05 10:40:25 +0100

    :(

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const searchInput = document.querySelector(".searcinput")
 const cancelBtn = document.querySelector(".cancel-btn")
 const searchBox = document.querySelector(".search-box")
 const account = document.querySelector(".account")
-const logo = document.querySelector(".logo")
+const logo = document.querySelector(".logo-link")
 const menuBtn = document.querySelector(".menu-btn")
-const menuBtn2 = document.querySelector(".menu-btn2")
 
 searchBtn.onclick = () =>{
   searchBtn.classList.add("active");
@@ -15,7 +14,6 @@ searchBtn.onclick = () =>{
   logo.classList.add("active");
   cancelBtn.classList.add("active");
   menuBtn.classList.add("active");
-  menuBtn2.classList.add("active");
 }
 
 cancelBtn.onclick = () =>{
@@ -25,5 +23,4 @@ cancelBtn.onclick = () =>{
   account.classList.remove("active");
   cancelBtn.classList.remove("active");
   menuBtn.classList.remove("active");
-  menuBtn2.classList.remove("active");
 }
